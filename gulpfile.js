140d43c05bde4a4e6dacfd570d9003aaaf71cede
Tim Petricola <tim.petricola@gmail.com>
2014-09-15 15:15:53 +0200

    Use Myth instead of Sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,21 @@
-var gulp    = require('gulp'),
-    sass    = require('gulp-ruby-sass');
-    rename  = require('gulp-rename');
+var gulp         = require('gulp'),
+    myth         = require('gulp-myth'),
+    minifyCSS    = require('gulp-minify-css'),
+    rename       = require('gulp-rename');
 
-var config = {
-  sassSrc: 'sass/**.scss',
-  cssDest: 'stylesheets',
-};
+gulp.task('css', function() {
+  gulp.src('src/styles.css')
+    .pipe(myth())
+    .pipe(gulp.dest('dist'));
 
-gulp.task('sass', function() {
-  gulp.src(config.sassSrc)
-    .pipe(sass({
-      compass: true,
-      style: 'compressed'
-    }))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest(config.cssDest));
-
-  gulp.src(config.sassSrc)
-    .pipe(sass({
-      compass: true,
-      style: 'nested'
-    }))
-    .pipe(gulp.dest(config.cssDest));
+  gulp.src('dist/styles.css')
+    .pipe(minifyCSS())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch(config.sassSrc, ['sass']);
+  gulp.watch('src/styles.css', ['css']);
 });
 
 gulp.task('default', ['watch']);

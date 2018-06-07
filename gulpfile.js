64750035b004587bfc8d5b4423151b1ef99dd716
Tim Petricola <tim.petricola@gmail.com>
2018-06-07 22:05:05 +0200

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,20 @@
-var gulp         = require('gulp'),
-    myth         = require('gulp-myth'),
-    minifyCSS    = require('gulp-minify-css'),
-    rename       = require('gulp-rename');
+var gulp = require("gulp"),
+  myth = require("gulp-myth"),
+  minifyCSS = require("gulp-minify-css"),
+  rename = require("gulp-rename");
 
-gulp.task('css', function() {
-  gulp.src('src/styles.css')
+gulp.task("css", function() {
+  return gulp
+    .src("src/styles.css")
     .pipe(myth())
-    .pipe(gulp.dest('dist'))
-    .on('end', function() {
-      gulp.src('dist/styles.css')
+    .pipe(gulp.dest("dist"))
+    .on("end", function() {
+      gulp
+        .src("dist/styles.css")
         .pipe(minifyCSS())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('dist'));
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(gulp.dest("dist"));
     });
-
 });
 
-gulp.task('watch', function() {
-  gulp.watch('src/styles.css', ['css']);
-});
-
-gulp.task('default', ['watch']);
+gulp.task("default", gulp.series("css"));
